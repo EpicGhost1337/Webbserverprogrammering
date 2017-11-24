1d0d7f3221e35e5af763a9e23e524d40b7d96978
EpicGhost1337 <andy.ung.te16d@gmail.com>
2017-11-24 10:44:09 +0100

    La in input om Användanamn och lösenord + doctype html

--- Uppgifter/Login/Login.php
+++ Uppgifter/Login/Login.php
@@ -1,3 +1,4 @@
+<doctype html>
 <html>
     
 <head>
@@ -5,8 +6,21 @@
 </head>
     
 <body>
+    <form>
+         Andvändanamn:
+         <input type = "text" name = "Username"><br>
+        
+         Lösenord:
+         <input type = "password" name = "password">
+         
+         <input type="submit" name = "Klick">
+         
+        
+        
     
+    </form>
 </body>
 
 
 </html>
+</doctype>
