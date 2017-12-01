0c8ff0c6558fd8512aff232ed16a79b07603bd8f
EpicGhost1337 <andy.ung.te16d@gmail.com>
2017-12-01 13:49:53 +0100

    Ändrade för att jag glömde aprostrof på variabel i query samtidig lagt till en ny variabel som ta svaren på frågan om man skrivit rätt Username,Password så får man "Successfully" om inte så är det "login falil"

--- Uppgifter/Login/Submit.php
+++ Uppgifter/Login/Submit.php
@@ -6,14 +6,25 @@ $Username = $_POST['Username'];
 $Password = $_POST['Password'];
 
 $query = "Select*From registera where Username = $Username AND Password = $Password;";
+$query = "Select * From registera where Username = '$Username' AND Password = '$Password';";
 
 mysqli_query($dbc,$query);
 
+$result = mysqli_query($dbc,$query);
 
 $query = "Select*from register where Username = $Username, Password = $Password;"
     
     mysqli_query($dbc,$query);
     echo $dbc;
     
+    if($row = mysqli_fetch_array($result)) {
+        
+        echo "Successfully logged in!";
+        
+    }
+        
+        echo "Login failed, try again!";
+        
+    }
   
 ?>
