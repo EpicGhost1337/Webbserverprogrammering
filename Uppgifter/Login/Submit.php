1f88e87f5572fea6f0c1fbf11ffde0ca984f68d9
EpicGhost1337 <andy.ung.te16d@gmail.com>
2017-12-01 10:13:19 +0100

    ger frågan till databaset som sedan kollar om det finns Username och Password

--- Uppgifter/Login/Submit.php
+++ Uppgifter/Login/Submit.php
@@ -6,4 +6,10 @@ $Username = $_POST['Username'];
 $Password = $_POST['Password'];
 
 
+$query = "Select*from register where Username = $Username, Password = $Password;"
+    
+    mysqli_query($dbc,$query);
+    echo $dbc;
+    
+  
 ?>
