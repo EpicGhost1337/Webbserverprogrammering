40195f299adc7618ef7c568d818968f14943fb6b
EpicGhost1337 <andy.ung.te16d@gmail.com>
2017-12-01 10:43:23 +0100

    korrekta rätt bokstäver och ställer en fråga till databasen som hämta Username och Password från databaset

--- Uppgifter/Login/Login.php
+++ Uppgifter/Login/Login.php
@@ -11,7 +11,7 @@
          <input type = "text" name = "Username"><br>
         
          Lösenord:
-         <input type = "password" name = "Password">
+         <input type = "Password" name = "Password">
          
          <input type="submit" name = "Klick">
          
--- Uppgifter/Login/Submit.php
+++ Uppgifter/Login/Submit.php
@@ -5,5 +5,9 @@ $dbc = mysqli_connect("localhost","root","","register");
 $Username = $_POST['Username'];
 $Password = $_POST['Password'];
 
+$query = "Select*From registera where Username = $Username AND Password = $Password;";
+
+mysqli_query($dbc,$query);
+
 
 ?>
