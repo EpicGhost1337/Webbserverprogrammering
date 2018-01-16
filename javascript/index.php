0c613f32501844c52cdb8fa35cdfd72f0690e3d6
EpicGhost1337 <andy.ung.te16d@gmail.com>
2018-01-16 17:27:03 +0100

    la till bild i mappen och gav att om man har musen över ska det bli större och om musen är utanför bilden bli den normal storlek

--- javascript/index.php
+++ javascript/index.php
@@ -15,6 +15,21 @@
              }
          </script>
     
+        <h2> Större bild</h2>
+           <img onmouseover="bigImg(this)" onmouseout="normalImg(this)" border="0" src="3.png" alt="think" width="32" height="32">
+            
+            <script>
+             function bigImg(x) {
+             x.style.height = "125px";
+             x.style.width = "125px";
+             }
+
+              function normalImg(x) {
+              x.style.height = "32px";
+              x.style.width = "32px";
+              }
+            </script>
+    
     </body>
 
 
