3a9d59aa0c642d87c5141e5462b12b6f4f49f9a4
EpicGhost1337 <andy.ung.te16d@gmail.com>
2018-10-05 13:19:09 +0200

    WebbServer

--- Dawei/Bordsbokning/index.php
+++ Dawei/Bordsbokning/index.php
@@ -4,12 +4,37 @@ include("../Templets/navigation.php");
 
 $dbc = mysqli_connect("localhost","root","","dawei");
 
+if(isset($_GET['book'])){
+    
+    $tables = explode("o",$_GET['book']);
+    
+    for($i = 0 ; $i < count($tables)  ; $i++){
+        
+        $bord_id = $tables[$i];
+        
+        $query = "SELECT * FROM BORD JOIN bokning ON bord.bord_id = bokning.bokning_bord where bord_id = $bord_id;";
+        $result = mysqli_query($dbc,$query);
+        if(!mysqli_fetch_array($result)){
+            echo "BOKA " . $bord_id . "<br>";
+            // SKAPA BOKNIG MED DATABASFRÅGA
+        }
+        else{
+            echo "FULLT " . $bord_id . "<br>";
+        }
+        
+        
+        
+    }
+    
+}
+
+
 if(isset($_POST['name']) && isset($_POST['date']) && isset($_POST['time']) && isset($_POST['personer']) && isset($_POST['extra'])){
-echo "Skickat";
+
    $Antal = $_POST['personer'];
-   $query= "SELECT * FROM BORD WHERE BORD_PLATSER > $Antal";
+   $query= "SELECT * FROM bord WHERE bord_platser > $Antal";
+    
    $result = mysqli_query($dbc,$query);
-  
     $Namn = $_POST['name'];
     $Tid = $_POST['time'];
     $Datum = $_POST['date'];
@@ -17,9 +42,9 @@ echo "Skickat";
     if(isset($_POST['extra'])){
        $extra=$_POST ['extra'];
     }
-   $query = "INSERT INTO bokning (bokning_namn,boking_datum,bokning_tid,bokning_antal, bokning_extra)
-   VALUES('$Namn','$Datum','$Tid',$Antal,'$Extra');";
-    mysqli_query($dbc,$query);
+   $query = "INSERT INTO bokning (bokning_namn, boking_datum, bokning_tid, bokning_antal, bokning_extra)
+   VALUES('$Namn','$Datum','$Tid','$Antal','$Extra');";
+   $result = mysqli_query($dbc,$query);
 }
 ?>
 
---
+++ Templates/navigation.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <link rel="stylesheet" href="../CSS/Css.css">
+     <link rel="stylesheet" href="../CSS/dish.css">
+     <link rel="stylesheet" href="../CSS/Hem.css">
+
+</head>
+<body>
+    <img id="Loga" scr="../imgs/loga.png">
+     <img id="Main" src="../imgs/Lila-bakgrund.png">
+    
+<ul>
+  <li><a class="active" href="/dawei/hem/">Hem</a></li>
+  <li><a href="/dawei/meny/">Meny</a></li>
+  <li><a href="/dawei/Bordsbokning">Bordsbokning</a></li>
+  <li><a href="/dawei/Omdömen">Omdömen</a></li>
+  <li><a href="/dawei/about/">Om oss</a></li>
+</ul>
+    
+    </body>    
+</html>
